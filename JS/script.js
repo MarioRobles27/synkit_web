1b33b9f4702ec6914ebd832a56494f8db434db2b
MarioRobles27 <marioscarobles@gmail.com>
2024-10-30 20:41:08 +0100

    new_pdp

--- JS/script.js
+++ JS/script.js
@@ -75,6 +75,32 @@ if ($('#menu-lateral').length > 0) {
 }
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    gsap.registerPlugin(Flip);
+
+    let products = gsap.utils.toArray(".product");
+    let active = products[0];
+
+    products.forEach(el => {
+        el.addEventListener("click", () => changeGrid(el));
+    });
+
+    function changeGrid(el) {
+        if (el === active) return;
+
+        let state = Flip.getState(products);
+        active.dataset.grid = el.dataset.grid;
+        el.dataset.grid = "img-1";
+        active = el;
+
+        Flip.from(state, {
+            duration: 0.3,
+            absolute: true,
+            ease: "power1.inOut"
+        });
+    }
+});
+
 
 // LIBRERIA AOS ACTUALIZACIONES
 window.onload = function () {
