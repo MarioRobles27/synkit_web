1d6a08d041b6207cb5136273a5aef086b1d8a0ca
MarioRobles27 <marioscarobles@gmail.com>
2024-10-30 12:12:44 +0100

    Menu_lateral_circulo

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,78 @@
-//animate burger icon
-$(document).ready(function() {
-    $(".burger").on("click",function() {
-      $(".burger").toggleClass("active");
+// //animate burger icon
+// $(document).ready(function() {
+//     $(".burger").on("click",function() {
+//       $(".burger").toggleClass("active");
       
-    });
-  });
-  //slide menu list
-  $(document).ready(function(){
-    $(".burger").click(function(){
-      $("ul").slideToggle("ul");
+//     });
+//   });
+//   //slide menu list
+//   $(document).ready(function(){
+//     $(".burger").click(function(){
+//       $("ul").slideToggle("ul");
       
-    });
+//     });
+//   });
+
+// MENÚ LATERAL
+// ------------
+function MenuLateral() {
+  var dis = this;
+  dis.menu = $('#menu-lateral');
+  dis.btn = $('#btn-menu-lateral');
+  
+  // Determino si puedo hacer touch o no.
+  dis.touchsi = 'ontouchstart' in window;
+
+  // Variables para guardar eventos.
+  dis.empieza = dis.touchsi ? 'touchstart' : 'mousedown';
+  dis.mientras = dis.touchsi ? 'touchmove' : 'mousemove';
+  dis.termina = dis.touchsi ? 'touchend' : 'mouseup';
+
+  dis.moviendo = false;
+  dis.puntoPartida;
+  dis.movido;
+  dis.pos;
+  dis.abierto = false;
+
+  // Evento de inicio
+  dis.menu.on(dis.empieza, function(event) {
+      event.preventDefault();
+
+      dis.moviendo = true;
+      dis.puntoPartida = dis.touchsi ? event.originalEvent.touches[0].clientX : event.clientX;
+      dis.pos = -dis.menu.position().left;
   });
+
+  // Evento de movimiento
+  $(document).on(dis.mientras, function(event) {
+      event.preventDefault();
+
+      if (dis.moviendo) {
+          dis.movido = (dis.touchsi ? event.originalEvent.touches[0].clientX : event.clientX) - dis.puntoPartida;
+          dis.menu.css('left', (dis.movido - dis.pos) + 'px');
+      }
+  });
+
+  // Evento de terminación
+  $(document).on(dis.termina, function(event) {
+      dis.moviendo = false;
+      event.preventDefault();
+
+      if (dis.movido > 50) {
+          dis.menu.css('left', '0px');
+          dis.abierto = true;
+      } else if (dis.movido < -50) {
+          dis.menu.css('left', '-220px');
+          dis.abierto = false;
+      } else {
+          dis.menu.css('left', dis.abierto ? '0px' : '-220px');
+      }
+  });
+}
+
+if ($('#menu-lateral').length > 0) {
+  var lateral = new MenuLateral();
+}
 
 
 // LIBRERIA AOS ACTUALIZACIONES
